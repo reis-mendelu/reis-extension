949c1fc4d5bdf5975b0d50e0ee9f13e7f2a71228
ElijaahInverted <holekdom@gmail.com>
2026-04-04 19:12:38 +0000

    chore: bump to 4.9.1 - clarify deadlines & improve tasks UI

--- wxt.config.ts
+++ wxt.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   manifest: {
     name: 'reIS',
-    version: '4.9.0',
+    version: '4.9.1',
     description: 'Modernizovaný reIS rozšířený pro IS Mendelu',
     icons: {
       16: 'reIS_logo_16.png',
