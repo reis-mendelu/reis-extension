c3b327b9d01e4999469e03a9a1932a15a68d47f5
Dominik - Personal <dominik-personal@Dominiks-MacBook-Air.local>
2026-06-21 12:13:12 +0200

    feat(map): remove BuildingBar (sibling outlines + click-to-exit replace it)

--- src/components/CampusMap/CampusMapView.tsx
+++ src/components/CampusMap/CampusMapView.tsx
@@ -1,5 +1,4 @@
 import { MapCanvas } from './MapCanvas';
-import { BuildingBar } from './BuildingBar';
 import { FloorStack } from './FloorStack';
 import { DetailPanel } from './DetailPanel';
 import { RoomSearch } from './RoomSearch';
@@ -11,7 +10,6 @@ export function CampusMapView() {
     <div className="relative w-full h-full">
       <MapCanvas />
       <div className="absolute top-3 left-3 z-[1000] flex items-center gap-2">
-        <BuildingBar />
         <RoomSearch />
       </div>
       <div className="absolute top-3 right-3 z-[1000]"><FloorStack /></div>
